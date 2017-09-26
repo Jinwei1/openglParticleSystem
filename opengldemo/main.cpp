0a4f711cc41428bb3d191cf4aca7585ec0dc33e2
J Yuan <jyuan@Js-MacBook-Pro.local>
2017-09-26 14:44:09 +0100

    Initial Commit

---
+++ opengldemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengldemo
+//
+//  Created by J Yuan on 26/9/17.
+//  Copyright © 2017 J Yuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
